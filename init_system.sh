d4652c7003779955e7a3717c54db30034cf9ccf4
rcoutinho <rcoutinho@192.168.1.6>
2021-10-25 09:28:48 -0300

    changed some repos

--- init_system.sh
+++ init_system.sh
@@ -3,17 +3,17 @@
 #adding keys
 sudo rpm --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 #adding repos
 sudo zypper addrepo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 sudo zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
 sudo zypper addrepo https://packages.microsoft.com/yumrepos/edge microsoft-edge-beta
 sudo zypper addrepo https://download.opensuse.org/repositories/openSUSE:Leap:15.3:NonFree:Update/standard/openSUSE:Leap:15.3:NonFree:Update.repo
-sudo zypper addrepo -cfp 90 -n VLC http://download.videolan.org/pub/vlc/SuSE/Tumbleweed/ vlc
-sudo zypper addrepo https://download.nvidia.com/opensuse/tumbleweed NVIDIA
+sudo zypper addrepo https://download.videolan.org/pub/vlc/SuSE/SLEap_15.3/ VLC
+sudo zypper addrepo https://download.nvidia.com/opensuse/leap/15.3 NVIDIA
+sudo zypper addrepo https://download.opensuse.org/repositories/server:/database/openSUSE_Leap_15.3/server:database.repo
 
-sudo zypper refresh -y
+sudo zypper refresh
 
 #installing
 sudo zypper install -y sublime-text
@@ -24,6 +24,10 @@ sudo zypper install -y steam
 sudo zypper install -y vlc vlc-codecs
 sudo zypper install -y x11-video-nvidiaG05
 sudo zypper install -y nvidia-glG05
+sudo zypper install -y suse-prime
+sudo zypper install -y kio-gdrive
+sudo zypper install -y java-11-openjdk-devel
+sudo zypper install -y mongodb
 sudo pip3 install avell-unofficial-control-center
 
 sudo zypper update -y
